d432d94c1b11d724a3fe92118e7ed86ab15129b9
Daniel <danielmj@me.com>
2016-03-16 18:34:54 -0700

    Added basic UI

---
+++ README.md
@@ -0,0 +1,8 @@
+# TAMS iOS App
+
+## Compile Requirements
+
+1. XCode
+
+To access data, you must have a valid url to the backend server. This data will be pulled as needed on their respective pages. 
+
---
+++ TAMS/TAMS/InitialTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  TAMS
+//
+//  Created by Daniel Jackson on 3/16/16.
+//  Copyright © 2016 Daniel Jackson. All rights reserved.
+//
+
+import UIKit
+
+class InitialTabBarController: UITabBarController {
+
+}
+
